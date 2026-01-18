858e0899cdae203ced48480d2a9b6b979d2eaac2
josiasdev <josiasmartins098@gmail.com>
2026-01-18 17:23:01 -0300

    feat: estrutura do projeto completo.

---
+++ src/test/java/com/github/josiasdev/url_shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.josiasdev.url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
