ffc5fd1eba6bde8a9c706e14e654339cd3770f93
josiasdev <josiasmartins098@gmail.com>
2026-02-18 16:57:06 -0300

    feat: estrutura da url, com o id e a url completa.

---
+++ src/main/java/com/github/josiasdev/url_shortener/entities/UrlEntity.java
@@ -0,0 +1,53 @@
+package com.github.josiasdev.url_shortener.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "urls")
+public class UrlEntity {
+
+    @Id
+    private String id;
+
+    private String fullUrl;
+
+    @Indexed(expireAfter = "0")
+    private LocalDateTime expiresAt;
+
+    public UrlEntity() {
+
+    }
+
+    public UrlEntity(String id, String fullUrl, LocalDateTime expiresAt) {
+        this.id = id;
+        this.fullUrl = fullUrl;
+        this.expiresAt = expiresAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullUrl() {
+        return fullUrl;
+    }
+
+    public LocalDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFullUrl(String fullUrl) {
+        this.fullUrl = fullUrl;
+    }
+
+    public void setExpiresAt(LocalDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+}
